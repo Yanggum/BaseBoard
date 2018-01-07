87f3b694985f393a3a2ee9dc141fb2c9d752cb17
Changwon Park <bak405@miksystem.com>
2018-01-07 11:01:57 +0900

    BaseComponent 세팅

---
+++ VueBoard/BaseComponent/SiteBaseControl.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseComponent
+{
+	public class SiteBaseControl : System.Web.UI.UserControl
+	{
+
+	}
+}
---
+++ VueBoard/VueBoard/Base/StaticHeader/CommonClientScript.ascx
@@ -0,0 +1,1 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="CommonClientScript.ascx.cs" Inherits="VueBoard.Base.StaticHeader.CommonClientScript" %>
